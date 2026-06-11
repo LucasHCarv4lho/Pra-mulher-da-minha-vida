b1ce2f10f09665874ade9ac46a818053ce8fa829
LucasDTS <139704064+LucasHCarv4lho@users.noreply.github.com>
2026-06-11 12:51:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ console.log("VERSAO 2026 TESTE");
 // ======================
 
 const images = [
-    "Imagem1.jpg",
-    "Imagem2.jpg",
     "Imagem3.jpg",
     "Imagem4.jpg",
+    "Imagem2.jpg",
     "Imagem5.jpg",
+    "Imagem1.jpg",
     "Imagem6.jpg"
 ];
 
